93dd2d0cd9558e880df4f8b86ab7fce579123cab
rfid19230 <58638987+rfid19230@users.noreply.github.com>
2020-03-26 21:42:04 +0300

    Update views.sql

--- Database/views.sql
+++ Database/views.sql
@@ -10,11 +10,34 @@ _________________________________________________________________________
 пользователю.
 */
 
+-- Представление о всех взятых вещах всеми пользователями за всё время
+
 CREATE OR REPLACE VIEW
 	tvview
 AS SELECT
-	u.u_id as tvv_uid, u.u_f as tvv_uf, u.u_io as tvv_uio, u.u_post as tvv_upost,
-	mv.mv_id as tvv_mvid, mv.mv_desc as tvv_mvdesc, mv.mv_closet as tvv_mvcloset,
+	u.u_id as tvv_uid, u.u_f as tvv_uf, u.u_io as tvv_uio, u.u_emailt as tvv_email,
+	mv.mv_id as tvv_mvid, mv.mv_type as tvv_mvtype, mv.mv_desc as tvv_mvdesc,
+	mv.mv_closet as tvv_mvcloset, mv.mv_cell as tvv_mvcell,
+	tv.tv_take as tvv_take, tv.tv_ret as tvv_ret
+FROM
+	users u,
+	mvalues mv,
+	tvalues tv
+WHERE
+	u.u_id = tv.tv_user AND mv.mv_id = tv.tv_value
+ORDER BY
+	tv.tv_take DESC;
+DISC;
+
+-- Представление о всех взятых вещах всеми пользователями которые еще не вернули
+
+CREATE OR REPLACE VIEW
+	tvview
+AS SELECT
+	u.u_id as tvv_uid, u.u_f as tvv_uf, u.u_io as tvv_uio,
+	u.u_emailt as tvv_email, mv.mv_id as tvv_mvid, 
+	mv.mv_type as tvv_mvtype, mv.mv_desc as tvv_mvdesc,
+	mv.mv_closet as tvv_mvcloset, mv.mv_cell as tvv_mvcell,
 	tv.tv_take as tvv_take, tv.tv_ret as tvv_ret
 FROM
 	users u,
@@ -24,7 +47,48 @@ WHERE
 	u.u_id = tv.tv_user
 	AND
 	mv.mv_id = tv.tv_value
+	AND tv.tv_ret is NULL
 ORDER BY
 	tv.tv_take DESC;
 DISC;
 
+
+-- представление о всех доступных вещах в шкафах
+____________________________________
+
+CREATE OR REPLACE VIEW
+	avvalues
+AS SELECT
+	mv.mv_id as id, mv.mv_type as type, mv.mv_desc, 
+	сs.cs_currval as currvalue, c.c_bld as bld_id,  
+	b.bld_name as bld_name, b.bld_section as section, 
+	cs.cs_cell as cell, c.c_floor as floor
+FROM
+	mvalues mv,
+	closets c,
+	cells cs,
+	buildings b
+WHERE
+	mv.mv_closet = cs.cs_closet AND mv.mv_cell = cs.cs_num AND cs.cs_closet = c.c_id AND c.c_bld = b.bld_id
+ORDER BY
+	mv.mv_id DESC;
+DISC;
+
+-- представление о всех ячейках в  шкафах в которых есть место
+____________________________________
+
+CREATE OR REPLACE VIEW
+	avclosets
+AS SELECT
+	c.c_id, c.c_cells, cs.cs_cell as cell, сs.cs_currval as currvalue,
+	cs.cs_maxval, c.c_bld as bld_id,  b.bld_name as bld_name, 
+	b.bld_section as section, c.c_floor as floor
+FROM
+	closets c,
+	cells cs,
+	buildings b
+WHERE
+	cs.cs_closet = c.c_id AND c.c_bld = b.bld_id AND cs.cs_maxval > cs.cs_currval
+ORDER BY
+	c.c_id DESC;
+DISC;
